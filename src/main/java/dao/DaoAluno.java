2024efcdcf1c464d4fa240634b04425698932979
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2024-05-15 12:12:20 -0300

    Metodo salvarALuno

--- src/main/java/dao/DaoAluno.java
+++ src/main/java/dao/DaoAluno.java
@@ -2,8 +2,11 @@ package dao;
 
 /*Classe responsavel pelas operações no banco de dados*/
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import conexao.SingleConnection;
+import model.ModelAluno;
 
 public class DaoAluno {
 	
@@ -16,5 +19,39 @@ public class DaoAluno {
 
 	/*Metodo para salvar alunos*/
 	
-	
+	public static void salvarAluno(ModelAluno aluno) {
+		
+		try {
+			
+			String sql = "insert into tbaluno (nome,telefone,cpf,cep,logradouro,numero,bairro,cidade,uf) values"
+					+ "(?,?,?,?,?,?,?,?,?)";
+			PreparedStatement insert = connection.prepareStatement(sql);
+			
+			insert.setString(1, aluno.getNome());
+			insert.setString(2, aluno.getTelefone());
+			insert.setString(3, aluno.getCpf());
+			insert.setString(4, aluno.getCep());
+			insert.setString(5, aluno.getLogradouro());
+			insert.setString(6,aluno.getNome());
+			insert.setString(7, aluno.getBairro());
+			insert.setString(8, aluno.getCidade());
+			insert.setString(9, aluno.getUf());
+			insert.execute();
+			
+			connection.commit();
+			
+		} catch (Exception e) {
+			
+			try {
+				
+				connection.rollback();
+				
+			} catch (SQLException e1) {
+				
+				e1.printStackTrace();
+			}
+			
+			e.printStackTrace();
+		}
+	}
 }
