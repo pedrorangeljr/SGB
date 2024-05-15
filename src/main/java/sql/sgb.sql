f4f40815b4dbeb5d5c0f9bb49e8ad0f5bdb6a4d4
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2024-05-15 11:09:33 -0300

    Servletaluno e DaoAluno

--- src/main/java/sql/sgb.sql
+++ src/main/java/sql/sgb.sql
@@ -10,3 +10,25 @@ create table login(
  constraint use_pk primary key(id)
  
 );
+
+ALTER TABLE login ADD UNIQUE (email);
+
+create table tbaluno (
+  
+   idAluno integer auto_increment primary key,
+   nome varchar(200),
+   telefone integer,
+   cpf varchar(200),
+   cep varchar(200),
+   logradouro varchar(200),
+   numero varchar(200),
+   bairro varchar(200),
+   cidade varchar(200),
+   uf varchar(50),
+   multas integer,
+   checkbox bool,
+   unique(cpf)
+   
+   );
+   
+ALTER TABLE tbaluno ADD UNIQUE (telefone);
